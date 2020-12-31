6b5548a55af18a39649f44a2059f7aa3b9863cae
samin <saminrahmankhan97@gmail.com>
2020-12-31 22:22:41 +0600

    new iptables

---
+++ scripts/iptables4.sh
@@ -0,0 +1,5 @@
+sudo sysctl net.ipv4.conf.all.forwarding=1
+
+sudo iptables -P FORWARD ACCEPT
+
+sudo iptables -t nat -I PREROUTING 1 -i wlp3s0 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.2
