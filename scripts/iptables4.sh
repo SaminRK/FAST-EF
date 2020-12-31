95a98153fa2bda058676b02d82e63d9676e8e250
samin <saminrahmankhan97@gmail.com>
2020-12-31 23:02:27 +0600

    iptables

--- scripts/iptables4.sh
+++ scripts/iptables4.sh
@@ -2,4 +2,5 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
+sudo iptables -t nat -I OUTPUT 1 -d 192.168.61.3 -j DNAT --to-destination 192.168.0.106
 sudo iptables -t nat -I PREROUTING 1 -i wlp3s0 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.2
