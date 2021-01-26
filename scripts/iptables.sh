f0d944c3ccd1ca68bd31e1a42c5c82f5d1fd2d8d
samin <saminrahmankhan97@gmail.com>
2021-01-26 20:30:08 +0600

    iptables.sh and iptables2.sh changed

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -2,4 +2,4 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
-sudo iptables -t nat -I OUTPUT 1 -d 192.168.61.0/26 -j DNAT --to-destination 192.168.0.106
+sudo iptables -t nat -I OUTPUT 1 -d 192.168.61.0/24 -j DNAT --to-destination 192.168.0.106
