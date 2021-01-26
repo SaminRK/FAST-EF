7c2cf86a1818b8dd69a379d9122f989bf8d7ad31
samin <saminrahmankhan97@gmail.com>
2021-01-26 13:21:02 +0600

    ue->enb->?

--- scripts/iptables2.sh
+++ scripts/iptables2.sh
@@ -2,4 +2,4 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
-sudo iptables -t nat -I PREROUTING 1 -i wlp3s0 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.3
+sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.0/24
--- scripts/setup_spgwc.sh
+++ scripts/setup_spgwc.sh
@@ -8,4 +8,5 @@ SECONDARY_DNS_IP_ADDRESS="8.8.4.4"
 python3 component/oai-spgwc/ci-scripts/generateConfigFiles.py --kind=SPGW-C \
           --s11c=eth0 --sxc=eth0 --apn=apn1.carrier.com \
           --dns1_ip=$DNS_IP_ADDRESS --dns2_ip=$SECONDARY_DNS_IP_ADDRESS \
-          --from_docker_file --env_for_entrypoint
+          --from_docker_file --env_for_entrypoint \
+          --push_protocol_option=yes
