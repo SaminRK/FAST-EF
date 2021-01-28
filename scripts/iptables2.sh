18f2610af79f6dbd8692af320f70e889532758f7
samin <saminrahmankhan97@gmail.com>
2021-01-28 20:02:51 +0600

    shunt iptables

--- scripts/iptables2.sh
+++ scripts/iptables2.sh
@@ -2,6 +2,6 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
-sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -p UDP--dport 2152 -j DNAT --to-destination 192.168.61.5
+sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -p UDP --dport 2152 -j DNAT --to-destination 192.168.61.5
 sudo iptables -t nat -I PREROUTING 2 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.3
 
