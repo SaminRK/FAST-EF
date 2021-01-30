8813ff13c313776adfa09564a6b9b8a6bc2b7952
samin <saminrahmankhan@gmail.com>
2021-01-31 00:46:23 +0600

    Dispatcher setup done

--- scripts/iptablest2.sh
+++ scripts/iptablest2.sh
@@ -2,7 +2,7 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
-sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -p UDP --dport 2152 -j DNAT --to-destination 192.168.61.5:7000
-sudo iptables -t nat -I PREROUTING 2 -s 192.168.0.0/24 -j DNAT --to-destination 192.168.61.3
-sudo iptables -t mangle -A PREROUTING -s 192.168.0.0/24 -j TEE --gateway 127.0.0.2
-sudo iptables -t nat -A PREROUTING -d 127.0.0.2 -j DNAT  --to 127.0.0.1:9000
+sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -d 192.168.0.106 -p UDP --dport 2152 -j REDIRECT --to-ports 7000
+sudo iptables -t nat -I PREROUTING 2 -s 192.168.0.0/24 -d 192.168.0.106 -j DNAT --to-destination 192.168.61.3
+# sudo iptables -t mangle -A PREROUTING -s 192.168.0.0/24 -j TEE --gateway 127.0.0.2
+# sudo iptables -t nat -A PREROUTING -d 127.0.0.2 -j DNAT  --to 127.0.0.1:9000
