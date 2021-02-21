778d7a3632ea46f2337cc1dc3afd25351bc6b997
samin <saminrahmankhan97@gmail.com>
2021-02-21 20:58:08 +0600

    dispatcher and controller complete; gets all information from attach

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -5,6 +5,6 @@ sudo iptables -P FORWARD ACCEPT
 # Samin
 EPC_HOST='192.168.0.106'
 # Sadman
-EPC_HOST='192.168.1.103'
+#EPC_HOST='192.168.1.103'
 
 sudo iptables -t nat -I OUTPUT 1 -d 192.168.61.0/26 -j DNAT --to-destination ${EPC_HOST}
