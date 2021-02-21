421a353257614fda9c12861da3c525883698e734
samin <saminrahmankhan@gmail.com>
2021-02-21 19:41:43 +0600

    stash

--- scripts/iptables3.sh
+++ scripts/iptables3.sh
@@ -2,5 +2,8 @@ sudo sysctl net.ipv4.conf.all.forwarding=1
 
 sudo iptables -P FORWARD ACCEPT
 
-sudo iptables -t nat -I PREROUTING 1 -s 192.168.63.2 -d 192.168.0.106 -j DNAT --to-destination 192.168.61.3
-sudo iptables -t nat -I OUTPUT 1 -d 192.168.63.2 -j DNAT --to-destination 192.168.0.108
+#sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -d 192.168.0.106 -p UDP --dport 2152 -j REDIRECT --to-ports 7000
+sudo iptables -t nat -I PREROUTING 1 -s 192.168.0.0/24 -d 192.168.0.106 -p SCTP --dport 36412 -j REDIRECT --to-ports 36414
+#sudo iptables -t nat -I PREROUTING 3 -s 192.168.0.0/24 -d 192.168.0.106 -j DNAT --to-destination 192.168.61.3
+#sudo iptables -t mangle -A PREROUTING -s 192.168.0.0/24 -j TEE --gateway 127.0.0.2
+#sudo iptables -t nat -A PREROUTING -d 127.0.0.2 -j DNAT  --to 127.0.0.1:9000
