9042a146c5da7f087cbe9cf65fa3f5602c1933d6
saadsakib <saadsakib3@gmail.com>
2021-03-24 22:23:40 +0600

    Update iptables92 for sending data to home sctp dispatcher

--- scripts/iptables92.sh
+++ scripts/iptables92.sh
@@ -14,4 +14,4 @@ sudo iptables -t nat -I PREROUTING 2 -s ${ENB_NETWORK} -d ${EPC_HOST} -p UDP --d
 # sudo iptables -t nat -I PREROUTING 1 -s ${ENB_NETWORK} -d ${EPC_HOST} -p UDP --dport 7152 -j DNAT --to-destination 192.168.61.5:2152
 # sudo iptables -t nat -I PREROUTING 2 -s ${ENB_NETWORK} -d ${EPC_HOST} -p UDP --dport 8152 -j DNAT --to-destination 192.168.61.9:2152
 sudo iptables -t nat -I PREROUTING 3 -s ${ENB_NETWORK} -d ${EPC_HOST} -p sctp --dport 16412 -j DNAT --to-destination ${EPC_HOST}:36414
-sudo iptables -t nat -I PREROUTING 4 -s ${ENB_NETWORK} -d ${EPC_HOST} -p sctp --dport 26412 -j DNAT --to-destination 192.168.61.7:36412
+sudo iptables -t nat -I PREROUTING 4 -s ${ENB_NETWORK} -d ${EPC_HOST} -p sctp --dport 26412 -j DNAT --to-destination ${EPC_HOST}:36415
