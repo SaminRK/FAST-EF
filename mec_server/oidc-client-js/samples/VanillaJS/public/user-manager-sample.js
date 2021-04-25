8c557f70753c685e081d7eaadfa640dd6df805e3
saadsakib <saadsakib3@gmail.com>
2021-04-25 11:05:25 +0600

    Change host address to localhost in oidc client

--- mec_server/oidc-client-js/samples/VanillaJS/public/user-manager-sample.js
+++ mec_server/oidc-client-js/samples/VanillaJS/public/user-manager-sample.js
@@ -4,21 +4,41 @@
 ///////////////////////////////
 // UI event handlers
 ///////////////////////////////
-document.getElementById('clearState').addEventListener("click", clearState, false);
-document.getElementById('getUser').addEventListener("click", getUser, false);
-document.getElementById('removeUser').addEventListener("click", removeUser, false);
+document
+  .getElementById("clearState")
+  .addEventListener("click", clearState, false);
+document.getElementById("getUser").addEventListener("click", getUser, false);
+document
+  .getElementById("removeUser")
+  .addEventListener("click", removeUser, false);
 
-document.getElementById('startSigninMainWindow').addEventListener("click", startSigninMainWindow, false);
-document.getElementById('endSigninMainWindow').addEventListener("click", endSigninMainWindow, false);
-document.getElementById('startSigninMainWindowDiffCallbackPage').addEventListener("click", startSigninMainWindowDiffCallbackPage, false);
+document
+  .getElementById("startSigninMainWindow")
+  .addEventListener("click", startSigninMainWindow, false);
+document
+  .getElementById("endSigninMainWindow")
+  .addEventListener("click", endSigninMainWindow, false);
+document
+  .getElementById("startSigninMainWindowDiffCallbackPage")
+  .addEventListener("click", startSigninMainWindowDiffCallbackPage, false);
 
-document.getElementById('popupSignin').addEventListener("click", popupSignin, false);
-document.getElementById('iframeSignin').addEventListener("click", iframeSignin, false);
+document
+  .getElementById("popupSignin")
+  .addEventListener("click", popupSignin, false);
+document
+  .getElementById("iframeSignin")
+  .addEventListener("click", iframeSignin, false);
 
-document.getElementById('startSignoutMainWindow').addEventListener("click", startSignoutMainWindow, false);
-document.getElementById('endSignoutMainWindow').addEventListener("click", endSignoutMainWindow, false);
+document
+  .getElementById("startSignoutMainWindow")
+  .addEventListener("click", startSignoutMainWindow, false);
+document
+  .getElementById("endSignoutMainWindow")
+  .addEventListener("click", endSignoutMainWindow, false);
 
-document.getElementById('popupSignout').addEventListener("click", popupSignout, false);
+document
+  .getElementById("popupSignout")
+  .addEventListener("click", popupSignout, false);
 
 ///////////////////////////////
 // config
@@ -26,27 +46,27 @@ document.getElementById('popupSignout').addEventListener("click", popupSignout, 
 Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.WARN;
 
-var hostAddr = '11.12.13.14';
+var hostAddr = "localhost";
 var appAddr = `${hostAddr}:15000`;
 var idpAddr = `${hostAddr}:15005`;
 
 var settings = {
-    authority: `http://${idpAddr}/oidc`,
-    client_id: 'js.tokenmanager',
-    redirect_uri: `http://${appAddr}/user-manager-sample.html`,
-    post_logout_redirect_uri: `http://${appAddr}/user-manager-sample.html`,
-    response_type: 'id_token token',
-    scope: 'openid email roles',
-    
-    popup_redirect_uri:`http://${appAddr}/user-manager-sample-popup-signin.html`,
-    popup_post_logout_redirect_uri:`http://${appAddr}/user-manager-sample-popup-signout.html`,
-    
-    silent_redirect_uri:`http://${appAddr}/user-manager-sample-silent.html`,
-    automaticSilentRenew:true,
-    //silentRequestTimeout:10000,
+  authority: `http://${idpAddr}/oidc`,
+  client_id: "js.tokenmanager",
+  redirect_uri: `http://${appAddr}/user-manager-sample.html`,
+  post_logout_redirect_uri: `http://${appAddr}/user-manager-sample.html`,
+  response_type: "id_token token",
+  scope: "openid email roles",
 
-    filterProtocolClaims: true,
-    loadUserInfo: true
+  popup_redirect_uri: `http://${appAddr}/user-manager-sample-popup-signin.html`,
+  popup_post_logout_redirect_uri: `http://${appAddr}/user-manager-sample-popup-signout.html`,
+
+  silent_redirect_uri: `http://${appAddr}/user-manager-sample-silent.html`,
+  automaticSilentRenew: true,
+  //silentRequestTimeout:10000,
+
+  filterProtocolClaims: true,
+  loadUserInfo: true,
 };
 var mgr = new Oidc.UserManager(settings);
 
@@ -54,119 +74,155 @@ var mgr = new Oidc.UserManager(settings);
 // events
 ///////////////////////////////
 mgr.events.addAccessTokenExpiring(function () {
-    console.log("token expiring");
-    log("token expiring");
+  console.log("token expiring");
+  log("token expiring");
 });
 
 mgr.events.addAccessTokenExpired(function () {
-    console.log("token expired");
-    log("token expired");
+  console.log("token expired");
+  log("token expired");
 });
 
 mgr.events.addSilentRenewError(function (e) {
-    console.log("silent renew error", e.message);
-    log("silent renew error", e.message);
+  console.log("silent renew error", e.message);
+  log("silent renew error", e.message);
 });
 
 mgr.events.addUserLoaded(function (user) {
-    console.log("user loaded", user);
-    mgr.getUser().then(function(){
-       console.log("getUser loaded user after userLoaded event fired"); 
-    });
+  console.log("user loaded", user);
+  mgr.getUser().then(function () {
+    console.log("getUser loaded user after userLoaded event fired");
+  });
 });
 
 mgr.events.addUserUnloaded(function (e) {
-    console.log("user unloaded");
+  console.log("user unloaded");
 });
 
 ///////////////////////////////
 // functions for UI elements
 ///////////////////////////////
-function clearState(){
-    mgr.clearStaleState().then(function(){
-        log("clearStateState success");
-    }).catch(function(e){
-        log("clearStateState error", e.message);
+function clearState() {
+  mgr
+    .clearStaleState()
+    .then(function () {
+      log("clearStateState success");
+    })
+    .catch(function (e) {
+      log("clearStateState error", e.message);
     });
 }
 
 function getUser() {
-    mgr.getUser().then(function(user) {
-        log("got user", user);
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .getUser()
+    .then(function (user) {
+      log("got user", user);
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function removeUser() {
-    mgr.removeUser().then(function() {
-        log("user removed");
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .removeUser()
+    .then(function () {
+      log("user removed");
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function startSigninMainWindow() {
-    mgr.signinRedirect({state:'some data'}).then(function() {
-        log("signinRedirect done");
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signinRedirect({ state: "some data" })
+    .then(function () {
+      log("signinRedirect done");
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function endSigninMainWindow() {
-    mgr.signinRedirectCallback().then(function(user) {
-        log("signed in", user);
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signinRedirectCallback()
+    .then(function (user) {
+      log("signed in", user);
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function startSigninMainWindowDiffCallbackPage() {
-    mgr.signinRedirect({state:'some data', redirect_uri: `http://${appAddr}/user-manager-sample-callback.html`}).then(function() {
-        log("signinRedirect done");
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signinRedirect({
+      state: "some data",
+      redirect_uri: `http://${appAddr}/user-manager-sample-callback.html`,
+    })
+    .then(function () {
+      log("signinRedirect done");
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function popupSignin() {
-    mgr.signinPopup({state:'some data'}).then(function(user) {
-        log("signed in", user);
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signinPopup({ state: "some data" })
+    .then(function (user) {
+      log("signed in", user);
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function popupSignout() {
-    mgr.signoutPopup({state:'some data'}).then(function() {
-        log("signed out");
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signoutPopup({ state: "some data" })
+    .then(function () {
+      log("signed out");
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 function iframeSignin() {
-    mgr.signinSilent({state:'some data'}).then(function(user) {
-        log("signed in", user);
-    }).catch(function(err) {
-        log(err);
+  mgr
+    .signinSilent({ state: "some data" })
+    .then(function (user) {
+      log("signed in", user);
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
-function startSignoutMainWindow(){
-    mgr.signoutRedirect({state:'some data'}).then(function(resp) {
-    //mgr.signoutRedirect().then(function(resp) {
-            log("signed out", resp);
-    }).catch(function(err) {
-        log(err);
+function startSignoutMainWindow() {
+  mgr
+    .signoutRedirect({ state: "some data" })
+    .then(function (resp) {
+      //mgr.signoutRedirect().then(function(resp) {
+      log("signed out", resp);
+    })
+    .catch(function (err) {
+      log(err);
     });
-};
+}
 
-function endSignoutMainWindow(){
-    mgr.signoutRedirectCallback().then(function(resp) {
-        log("signed out", resp);
-    }).catch(function(err) {
-        log(err);
+function endSignoutMainWindow() {
+  mgr
+    .signoutRedirectCallback()
+    .then(function (resp) {
+      log("signed out", resp);
+    })
+    .catch(function (err) {
+      log(err);
     });
-};
+}
