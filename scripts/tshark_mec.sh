575b4da8d82d4849b9e36fd61b8371df3a93a431
saadsakib <saadsakib3@gmail.com>
2021-04-30 20:00:11 +0600

    Update format of pcap files

--- scripts/tshark_mec.sh
+++ scripts/tshark_mec.sh
@@ -2,12 +2,12 @@ ne1='ip netns exec ns1'
 ne2='ip netns exec ns2'
 
   
-TIME_DIR="../archives/`date +%F`"
+TIME_DIR="../archives/`date +%m-%d`_$1_mec"
 
 mkdir -p $TIME_DIR
 
-HOME_LOG_FILE="$TIME_DIR/home_`date +%F`_$1.pcap"
-FOREIGN_LOG_FILE="$TIME_DIR/foreign_`date +%F`_$1.pcap"
+HOME_LOG_FILE="$TIME_DIR/home_`date +%m-%d`_$1.pcap"
+FOREIGN_LOG_FILE="$TIME_DIR/foreign_`date +%m-%d`_$1.pcap"
 
 sudo $ne2 tshark -ni any -f 'not (udp port 5353 or udp port 1900 or arp)' -w $HOME_LOG_FILE &
 sudo $ne1 tshark -ni any -f 'not (udp port 5353 or udp port 1900 or arp)' -w $FOREIGN_LOG_FILE
--- scripts/tshark_ue.sh
+++ scripts/tshark_ue.sh
@@ -1,7 +1,7 @@
-TIME_DIR="../archives/`date +%F`"
+TIME_DIR="../archives/`date +%m-%d`_$1_enb_ue"
 
 mkdir -p $TIME_DIR
 
-UE_PCAP_FILE="$TIME_DIR/ue_`date +%F`_$1.pcap"
+UE_PCAP_FILE="$TIME_DIR/ue_`date +%m-%d`_$1.pcap"
 
 sudo tshark -ni oaitun_ue1 -f 'not (udp port 5353 or udp port 1900 or arp)' -w $UE_PCAP_FILE
