f0f5f6756b2077bbfd5c5c662f33becacf1d1411
saadsakib <saadsakib3@gmail.com>
2021-05-02 08:40:54 +0600

    Use localhost for redirect uri in frontend

--- MEC/mec_app/frontend_vanilla/src/constants.js
+++ MEC/mec_app/frontend_vanilla/src/constants.js
@@ -1,6 +1,6 @@
 // export const IDP_ADDR = "http://104.196.145.129:15005";
 export const IDP_ADDR = "http://192.168.21.2:15005";
 
-export const REDIRECT_URI = "http://192.168.21.2:8080";
+export const REDIRECT_URI = "http://localhost:8080";
 
 export const API_URL = "http://192.168.21.2:4000";
